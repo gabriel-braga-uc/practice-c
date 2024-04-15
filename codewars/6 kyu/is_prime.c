124d7722732d5a19c40281d1d7a56bab4b0b0590
gabriel-braga-uc <126345318+gabriel-braga-uc@users.noreply.github.com>
2024-04-14 21:43:48 -0300

    Create is_prime.c

---
+++ codewars/6 kyu/is_prime.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+bool is_prime(int num)
+{
+  if(num > 2){
+    int i = 2;
+    while (i*i <= num){
+      if(num % i == 0){
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+  else{
+    return num == 2 ? true : false;
+  }
+}
